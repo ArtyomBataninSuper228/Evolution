1d512630636bef2296b41edef46018540bf95d5b
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-10-11 13:52:51 +0500

    commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import random
 from math import *
 import numpy as np
 import pygame as pg
+import copy
 
 organizms = []
-simulation_time = 0# время с начала симуляции
-h = 1/100# время между итерациями модели
+simulation_time = 0  # время с начала симуляции
+h = 1 / 100  # время между итерациями модели
 
 
 class Organizm:
-    def __init__(self, x, y, health = 100, energy = 100, speed = 10, color = (0,0,0), generation = 0, type = "plant", radius_of_view = 100, parents = [], gender = "male", icon = None):
+    def __init__(self, x=0, y=0, health=100, energy=100, speed=10, color=(0, 0, 0), generation=0, type="plant",
+                 radius_of_view=100, parents=[], gender="male", icon=None):
         global organizms, simulation_time
         self.x = x
         self.y = y
@@ -21,17 +24,61 @@ class Organizm:
         self.radius_of_view = radius_of_view
         self.time_of_birth = simulation_time
         self.age = 0
+        self.age_of_adult = 60
+        self.maxage = 60*60
         self.parents = parents
         self.gender = gender
         self.icon = icon
+        self.previous_move = [0, 0]
         organizms.append(self)
+
     def go(self, vector):
         global h
-        self.x += vector[0]*self.speed*h
-        self.y  += vector[1] * self.speed*h
-class Ship (Organizm):
-    def __init__(self, x, y, class_gender):
-        super.__init__(x, y, gender = class_gender)
+        self.x += vector[0] * self.speed * h
+        self.y += vector[1] * self.speed * h
+
+
+class Ship(Organizm):
+    def __init__(self, x, y, gender):
+        super().__init__(x = x, y= y, gender= gender)
         self.brave = 0
 
+    def analyzing(self):
+        enemies = []
+        plants = []
+        partners = []
+        ships = []
+        for organizm in organizms:
+            if sqrt((organizm.x - self.x)**2 + (organizm.y - self.y)**2) <= self.radius_of_view:
+                if (self.age < self.age_of_adult / 2) and(type(organizm) != Ship and type(organizm) != Plant):
+                    enemies.append(organizm)
+                elif (self.age < self.age_of_adult / 2) and  type(organizm) == Wolf:
+                    enemies.append(organizm)
+                if type(organizm) == Plant:
+                    plants.append(organizm)
+                if type(organizm) == Ship:
+                    ships.append(organizm)
+                    if organizm.gender != self.gender:
+                        partners.append(organizm)
+            move = copy.copy(self.previous_move)
+            angle = random.randint(0, 360)
+            move[0] = (move[0] + 0.01*sin(angle/180*pi))/1.01
+            move[1] = (move[1] + 0.01*cos(angle/180*pi))/1.01
 
+
+class Wolf(Organizm):
+    def __init__(self, x, y, gender):
+        super().__init__(x = x, y= y, gender= gender)
+        self.brave = 0
+
+    def analyzing(self):
+        for organizm in organizms:
+            pass
+class Plant(Organizm):
+    def __init__(self, x, y):
+        super().__init__(x = x, y= y)
+
+
+
+a = Ship(0,0, 'male')
+print(type(a))
---
+++ test.py
@@ -0,0 +1,9 @@
+import random
+from math import *
+from random import *
+
+a = [2**0.5/2, 2**0.5/2]
+for i in range(1000):
+    a[0] = (a[0] + )
+    a[1] = (a[1] + 0.1 * cos(ang / 180 * pi)) / 1.1
+print(a, (a[0]**2 + a[1]**2)**0.5)
